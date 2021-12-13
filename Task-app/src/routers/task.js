a748c5e95b295ea79078bbf849169a7e82c9a851
ashutoshpipriye <ashutoshpipriye123@gmail.com>
2021-12-14 01:58:54 +0530

    filter-sort-pagination-and-timestamps

--- Task-app/src/routers/task.js
+++ Task-app/src/routers/task.js
@@ -19,12 +19,34 @@ router.post("/tasks", auth, async (req, res) => {
 });
 
 // get all tasks
+// GET /tasks?completed=true
+// GET /tasks?limit=2&skip=2
+// GET /tasks?sortBy=createdAt:desc
 router.get("/tasks", auth, async (req, res) => {
   try {
-    // const tasks = await Task.find({ owner: req.user._id });
-    // res.send(tasks);
-    // await req.user.populate("tasks").execPopulate();  // In latest mongo version, populate method will do the job there is no need of execPopulate
-    await req.user.populate("tasks");
+    const match = {};
+    const sort = {};
+
+    // filter the data
+    if (req.query.completed) {
+      match.completed = req.query.completed === "true";
+    }
+
+    // sort the data
+    if (req.query.sortBy) {
+      const parts = req.query.sortBy.split(":");
+      sort[parts[0]] = parts[1] === "desc" ? -1 : 1;
+    }
+
+    await req.user.populate({
+      path: "tasks",
+      match,
+      options: {
+        limit: parseInt(req.query.limit),
+        skip: parseInt(req.query.skip),
+        sort,
+      },
+    });
     res.send(req.user.tasks);
   } catch (e) {
     res.status(500).send(e);
@@ -92,3 +114,7 @@ router.delete("/tasks/:id", auth, async (req, res) => {
 });
 
 module.exports = router;
+
+// const tasks = await Task.find({ owner: req.user._id });
+// res.send(tasks);
+// await req.user.populate("tasks").execPopulate();  // In latest mongo version, populate method will do the job there is no need of execPopulate
